920bcffa36bc4c9ef9a64b716b597b60c4521e65
Traceur0 <ghost01ghost@gmail.com>
2022-05-17 16:37:29 +0900

    template problem

--- kakao_login.py
+++ kakao_login.py
@@ -5,7 +5,7 @@ import json
 
 url = "https://kauth.kakao.com/oauth/token" 
 
-with open('./plaintext/key.json', 'r') as key_file:
+with open("./plaintext/key.json", "r") as key_file:
     key_json = json.load(key_file)
 key_K = key_json["kakaoTalk"]["kakao_key"]
 authorization_code = key_json["kakaoTalk"]["authorization_code"]
@@ -19,7 +19,7 @@ URL = "https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=1
 city_name = "seoul"
 lang_code = "kr"
 
-with open('./plaintext/key.json', 'r') as key_file:
+with open("./plaintext/key.json", "r") as key_file:
     key_json = json.load(key_file)
 key_O = key_json["openWeather"]["openWeather_key"]
 open_wthr_URL = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key_O}&lang={lang_code}&units=metric"
@@ -27,7 +27,7 @@ open_wthr_URL = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&
 # Naver "오늘 서울 날씨" searchedInfo
 basic_info = requests.get(URL)
 parsing = BeautifulSoup(basic_info.text ,"html.parser")
-wthr_today = parsing.select_one('div.temperature_text').text
+wthr_today = parsing.select_one("div.temperature_text").text
 
 # OpenWeather api weatherInfo
 open_wthr = requests.get(open_wthr_URL).text
@@ -38,11 +38,11 @@ OW_dic_wthr = OW_json["weather"][0]
 
 def Issue_token():
     data = {
-        'grant_type' : 'refresh_token',
-        'client_id' : key_K,
-        'redirect_URI' : "https://example.com/oauth",
-        'code' : authorization_code,
-        'refresh_token' : rf_token,
+        "grant_type" : "refresh_token",
+        "client_id" : key_K,
+        "redirect_URI" : "https://example.com/oauth",
+        "code" : authorization_code,
+        "refresh_token" : rf_token,
     }
     # request URL / data : additional requestInfo(parameter)
     rqst_URL = requests.post(url, data=data)
@@ -67,50 +67,21 @@ def Issue_token():
     result = token_read["access_token"]
     return result
 
-def Send_message():
-    with open("./plaintext/k_token.json", "r") as access_t:
-        acs_token = json.load(access_t)
-
+def Send_message(access_token):
     msg_sending_URL = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
 
     headers = {
-        "Authorizaion" : "Bearer " + acs_token["access_token"]
+        'Authorization' : "Bearer " + access_token
     }
 
-    data_raw = {
-        "object_type" : "feed",
-        "content" : "Today's WeatherForecast", #img
-        "item_content" : {
-            "profile_text" : "오늘의 날씨",
-            "title_image_text" : OW_dic_wthr["description"],
-            "title_image_category" : OW_dic_wthr["main"],
-            "items" : [
-                {
-                    "item" :"평균기온",
-                    "item_op" : str(OW_dic_main["temp"]) + "°C"
-                },
-                {
-                    "item" :"체감기온",
-                    "item_op" : str(OW_dic_main["feels_like"]) + "°C"
-                },
-                {
-                    "item" :"최저기온",
-                    "item_op" : str(OW_dic_main["temp_min"]) + "°C"
-                },
-                {
-                    "item" :"최고기온",
-                    "item_op" : str(OW_dic_main["temp_max"]) + "°C"
-                }
-            ] 
-        },
-        "buttons" : [
-            {
-                "title" : "네이버 날씨",
-                "link" : "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=오늘+서울+날씨"
+    data = { "template_object" : json.dumps({
+            "object_type" : "text",
+            "text" : "weatherForecast",
+            "link" : {
+                "web_url" : "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=오늘+서울+날씨"
             }
-        ]
+        })
     }
 
-    data = {'template_object' : json.dumps(data_raw, ensure_ascii=False)}
-    msg_rqst = requests.post(url, headers=headers, data=data)
-    return msg_rqst
+    msg_rqst = requests.post(msg_sending_URL, headers=headers, data=data)
+    return print(msg_rqst.content, access_token)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from kakao_login import Issue_token, Send_message
 
 token = Issue_token()
-print(token)
-Send_message()
+Send_message(token)
